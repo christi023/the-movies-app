9c434c2ed15272df285f70986f41c7926a93a1e0
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-07-15 23:20:34 +0200

    added searchbar

---
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+import { StyledSearchBar, StyledSearchBarContent } from '../styles/SearchBarStyles';
+import SearchIcon from '@material-ui/icons/Search';
+
+const SearchBar = ({ callback }) => {
+  const [state, setState] = useState('');
+  const timeOut = useRef(null);
+
+  const doSearch = (event) => {
+    const { value } = event.target;
+
+    clearTimeout(timeOut.current);
+    setState(value);
+
+    timeOut.current = setTimeout(() => {
+      callback(value);
+    }, 500);
+  };
+
+  return (
+    <StyledSearchBar>
+      <StyledSearchBarContent>
+        <SearchIcon className="fa-search" name="search" size="2x" />
+        <input type="text" placeholder="Search Movie" onChange={doSearch} value={state} />
+      </StyledSearchBarContent>
+    </StyledSearchBar>
+  );
+};
+
+SearchBar.propTypes = {
+  callback: PropTypes.func,
+};
+
+export default SearchBar;
