58240a4fde206ac01117e3ec2a2ed6e686f32eeb
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-07-19 14:51:17 +0200

    Movie info for movie

--- client/src/Components/MovieDetail/Sections/MovieInfo.jsx
+++ client/src/Components/MovieDetail/Sections/MovieInfo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Descriptions } from 'antd';
 
 function MovieInfo(props) {
@@ -8,16 +9,14 @@ function MovieInfo(props) {
     <Descriptions title="Movie Info" bordered>
       <Descriptions.Item label="Title">{movie.original_title}</Descriptions.Item>
       <Descriptions.Item label="release_date">{movie.release_date}</Descriptions.Item>
+      <Descriptions.Item label="original_language">{movie.original_language}</Descriptions.Item>
       <Descriptions.Item label="revenue">{movie.revenue}</Descriptions.Item>
       <Descriptions.Item label="runtime">{movie.runtime}</Descriptions.Item>
-      <Descriptions.Item label="vote_average" span={2}>
-        {movie.vote_average}
-      </Descriptions.Item>
+      <Descriptions.Item label="vote_average">{movie.vote_average}</Descriptions.Item>
       <Descriptions.Item label="vote_count">{movie.vote_count}</Descriptions.Item>
       <Descriptions.Item label="status">{movie.status}</Descriptions.Item>
       <Descriptions.Item label="popularity">{movie.popularity}</Descriptions.Item>
     </Descriptions>
   );
 }
-
 export default MovieInfo;
