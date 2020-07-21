796e2cf03711e6c22ecc8d29d6e71e8655ce5d88
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-07-21 19:42:40 +0200

    created movie grid cards for landing page

--- client/src/Components/Commons/GridCards.js
+++ client/src/Components/Commons/GridCards.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import { Card, Avatar, Col, Typography, Row } from 'antd';
-import { IMAGE_BASE_URL } from '../utils/misc';
+import PropTypes from 'prop-types';
 
-export default function GridCards(props) {
-  let { actor, key, image, movieId, movieName, characterName } = props;
-  const POSTER_SIZE = 'w154';
+import { GridStyle, GridContentStyle } from '../styles/GridStyle';
 
-  if (actor) {
-    return (
-      <Col key={key} lg={6} md={8} xs={24}>
-        <div style={{ position: 'relative' }}>
-          <img
-            style={{ width: '100%', height: '320px' }}
-            alt={characterName}
-            src={`${IMAGE_BASE_URL}${POSTER_SIZE}${image}`}
-          />
-        </div>
-      </Col>
-    );
-  } else {
-    return (
-      <Col key={key} lg={6} md={8} xs={24}>
-        <div style={{ position: 'relative' }}>
-          <a href={`/movie/${movieId}`}>
-            <img style={{ width: '100%', height: '320px' }} alt={movieName} src={image} />
-          </a>
-        </div>
-      </Col>
-    );
-  }
-}
+const GridCards = ({ header, children }) => (
+  <GridStyle>
+    <h1>{header}</h1>
+    <GridContentStyle>{children}</GridContentStyle>
+  </GridStyle>
+);
+
+GridCards.propTypes = {
+  header: PropTypes.string,
+};
+
+export default GridCards;
