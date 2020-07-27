9ff11125b78c4ec8cef3ed68a043e25cc2f0f28b
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-07-27 21:45:03 +0200

    Actor grids with styles

--- client/src/Components/Actor/Actor.jsx
+++ client/src/Components/Actor/Actor.jsx
@@ -6,14 +6,14 @@ import NoImage from '../../assets/images/no_image.jpg';
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../utils/misc';
 import { ActorStyle } from '../styles/ActorStyle';
 
-const Actor = ({ actor }) => (
+const Actor = ({ cast }) => (
   <ActorStyle>
     <img
-      src={actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage}
+      src={cast.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${cast.profile_path}` : NoImage}
       alt="actorThumb"
     />
-    <span className="actor-name">{actor.name}</span>
-    <span className="actor-character">{actor.character}</span>
+    <span className="actor-name">{cast.name}</span>
+    <span className="actor-character">{cast.character}</span>
   </ActorStyle>
 );
 
