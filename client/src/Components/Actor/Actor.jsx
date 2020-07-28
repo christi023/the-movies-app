22494cc476cd169d5571b1bc1f0f461a9925746a
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-07-28 15:47:07 +0200

    Actor grids with styles

--- client/src/Components/Actor/Actor.jsx
+++ client/src/Components/Actor/Actor.jsx
@@ -6,16 +6,22 @@ import NoImage from '../../assets/images/no_image.jpg';
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../utils/misc';
 import { ActorStyle } from '../styles/ActorStyle';
 
-const Actor = ({ cast }) => (
-  <ActorStyle>
-    <img
-      src={cast.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${cast.profile_path}` : NoImage}
-      alt="actorThumb"
-    />
-    <span className="actor-name">{cast.name}</span>
-    <span className="actor-character">{cast.character}</span>
-  </ActorStyle>
-);
+function Actor(props) {
+  let { actor, image, characterName } = props;
+
+  if (actor) {
+    return (
+      <ActorStyle>
+        <img
+          alt={characterName}
+          src={image ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage}
+        />
+        <span className="actor-name">{actor.name}</span>
+        <span className="actor-character">{actor.character}</span>{' '}
+      </ActorStyle>
+    );
+  }
+}
 
 Actor.propTypes = {
   actor: PropTypes.object,
