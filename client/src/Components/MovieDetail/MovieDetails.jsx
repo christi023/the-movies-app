631fc44df3b20f18ff69cda2243182640261ef3c
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-07-28 15:49:06 +0200

    Holds Movie info

--- client/src/Components/MovieDetail/MovieDetails.jsx
+++ client/src/Components/MovieDetail/MovieDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Button } from 'antd';
+import { Button } from 'antd';
 import { API_URL, API_KEY, IMAGE_BASE_URL, IMAGE_SIZE } from '../utils/misc';
 // axios
 import axios from 'axios';
@@ -11,7 +11,7 @@ import Favorite from './Sections/Favorite';
 import Spinner from '../Spinner/Spinner';
 //Grid Card component
 import GridCards from '../Commons/GridCards';
-
+import Actor from '../Actor/Actor';
 // Main Image Component
 import MainImage from '../LandingPage/Images/MainImage';
 
@@ -25,9 +25,10 @@ export default function MovieDetails(props) {
   const [LoadingForMovie, setLoadingForMovie] = useState(true);
   const [LoadingForCasts, setLoadingForCasts] = useState(true);
   const [ActorToggle, setActorToggle] = useState(false);
-  const movieVariable = { movieId: movieId };
+  const movieVariable = {
+    movieId: movieId,
+  };
 
-  // useEffect
   useEffect(() => {
     let endpointForMovieInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`;
     fetchDetailInfo(endpointForMovieInfo);
@@ -40,42 +41,22 @@ export default function MovieDetails(props) {
       } else {
         alert('Failed to get comments Info');
       }
-    }); // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [setMovie]);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  // params that matches movie poster
-  useEffect(() => {
-    const movieId = props.match.params.movieId;
-
-    fetch(`${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`)
-      .then((response) => response.json())
-      .then((response) => {
-        setMovie(response);
-
-        fetch(`${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`)
-          .then((response) => response.json())
-          .then((response) => {
-            console.log(response);
-            setCasts(response.cast);
-          });
-      });
-  }, [props.match.params.movieId]);
-
-  // viewing actors
   const toggleActorView = () => {
     setActorToggle(!ActorToggle);
   };
 
-  // fetching the detail info
   const fetchDetailInfo = (endpoint) => {
     fetch(endpoint)
       .then((result) => result.json())
       .then((result) => {
         console.log(result);
+        setMovie(result);
         setLoadingForMovie(false);
-        setMovie(Movie || result.results[0]);
 
-        // Casts endpoint results
         let endpointForCasts = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
         fetch(endpointForCasts)
           .then((result) => result.json())
@@ -83,13 +64,12 @@ export default function MovieDetails(props) {
             console.log(result);
             setCasts(result.cast);
           });
-        // set loading for casts
+
         setLoadingForCasts(false);
       })
       .catch((error) => console.error('Error:', error));
   };
 
-  // updating comments
   const updateComment = (newComment) => {
     setCommentLists(CommentLists.concat(newComment));
   };
@@ -110,40 +90,35 @@ export default function MovieDetails(props) {
         <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
           <Favorite movieInfo={Movie} movieId={movieId} userFrom={localStorage.getItem('userId')} />
         </div>
-
         {/* Movie Info */}
         {!LoadingForMovie ? <MovieInfo movie={Movie} /> : <Spinner />}
-
         <br />
         {/* Actors Grid*/}
-
         <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
           <Button onClick={toggleActorView}>Toggle Actor View </Button>
         </div>
-
-        {ActorToggle && (
-          <Row gutter={[16, 16]}>
-            {!LoadingForCasts ? (
-              Casts &&
-              Casts.map(
-                (cast, index) =>
-                  cast.profile_path && (
-                    <React.Fragment key={index}>
-                      <GridCards actor image={cast.profile_path} characterName={cast.character} />
-                    </React.Fragment>
-                  ),
-              )
-            ) : (
-              <Spinner />
-            )}
-          </Row>
-        )}
+        {ActorToggle &&
+          (!LoadingForCasts ? (
+            <GridCards header="Actors">
+              {Casts &&
+                Casts.map((actor, index) => (
+                  <React.Fragment key={index}>
+                    <Actor
+                      actor={actor}
+                      characterName={actor.character}
+                      image={actor.profile_path}
+                    />
+                  </React.Fragment>
+                ))}
+            </GridCards>
+          ) : (
+            <Spinner />
+          ))}
+        )
         <br />
-
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <LikeDislikes video videoId={movieId} userId={localStorage.getItem('userId')} />
         </div>
-
         {/* Comments */}
         <Comments
           movieTitle={Movie.original_title}
